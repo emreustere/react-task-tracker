70ee7dae42152293ac50aabe3cf22448fa227a4e
rippy56 <ustereemre@gmail.com>
2022-05-27 21:36:31 +0300

    Finished project commit.

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,13 +3,13 @@ import { FaTimes } from "react-icons/fa";
 const Task = ({ task, onDelete, onToggle }) => {
   return (
     <div
-      className={`task ${task.reminder ? "reminder" : ""} `}
+      className={`task ${task.reminder && "reminder"}`}
       onDoubleClick={() => onToggle(task.id)}
     >
       <h3>
         {task.text}{" "}
         <FaTimes
-          style={{ color: "steelblue", cursor: "pointer" }}
+          style={{ color: "red", cursor: "pointer" }}
           onClick={() => onDelete(task.id)}
         />
       </h3>
